ecdd2e506f9f74cf1e5ac8755b14cb80417f3031
Jonathan Driessen <jonathan-driessen@hotmail.com>
2018-06-18 19:20:02 +0200

    Ditch frontent. Just use this for calcing hands.

--- BackEnd/relation_checkers.py
+++ BackEnd/relation_checkers.py
@@ -3,24 +3,4 @@ def check_all_relations(hand, prizes, deck, relations):
         if eval(relation):
             # print(hand)
             relations[relation] += 1
-
-
-def is_in_hand(card, hand):
-    if card in hand:
-        return True
-    else:
-        return False
-
-
-def is_in_prizes(card, prizes):
-    if card in prizes:
-        return True
-    else:
-        return False
-
-
-def is_in_deck(card, deck):
-    if card in deck:
-        return True
-    else:
-        return False
+			
